e4feff7f6625e18e1a4e3e5625b7fd865428fe49
António Lopes <still_running_uphill@protonmail.com>
2025-04-08 09:47:27 +0100

    Added python script for API testing.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# api_4_llm
-A very simple Python API to control access to a LLM or an AI model.
+# API_4_LLMS
+
+- A very simple Python API to control access to a LLM or an AI model.
+- Based on the TWT's video tutorial https://www.youtube.com/watch?v=cy6EAp4iNN4
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Optional
 from fastapi import FastAPI, Depends, HTTPException, Header
 import ollama
 import os
@@ -6,15 +6,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_KEYS_CREDITS: dict[str | None, int] = {
+API_KEYS_CREDITS: dict[Optional[str], int] = {
     os.getenv(key="API_KEY"): 5
 }  # number of credits for each API key
+print(API_KEYS_CREDITS)
 
 app = FastAPI()
 
 
-def verify_api_key(x_api_key: str = Header(default=None)):
-    credits: int = API_KEYS_CREDITS.get(x_api_key, 0)
+def verify_api_key(x_api_key: str = Header(default=None)) -> str:
+    credits = API_KEYS_CREDITS.get(x_api_key, 0)
     if credits <= 0:
         raise HTTPException(
             status_code=401, detail="Invalid API key, or no credits left"
@@ -24,7 +25,7 @@ def verify_api_key(x_api_key: str = Header(default=None)):
 
 
 @app.post(path="/generate")
-def generate(prompt: str, x_api_key: str = Depends(dependency=verify_api_key)) -> dict[str, Any]:
+def generate(prompt, x_api_key = Depends(verify_api_key)) -> dict:
     API_KEYS_CREDITS[x_api_key] -= 1
     response: ollama.ChatResponse = ollama.chat(
         model="mistral", messages=[{"role": "user", "content": prompt}]
